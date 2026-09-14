b450bc40730d601c4f461a3d9f9b0fc81ee42e00
bobzap66 <nickgpfs@gmail.com>
2026-09-14 09:00:54 -0400

    Load Kingmaker vignette Explorer order

--- quartz/components/ExplorerCurator.tsx
+++ quartz/components/ExplorerCurator.tsx
@@ -1,9 +1,11 @@
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
 // @ts-expect-error - Inline script loaded as text by esbuild plugin
 import curatorScript from "./scripts/curatedExplorer.inline"
+// @ts-expect-error - Inline script loaded as text by esbuild plugin
+import kingmakerVignetteOrder from "./scripts/curatedExplorer.kingmakerVignettes.inline"
 
 export default (() => {
   const ExplorerCurator: QuartzComponent = () => null
-  ExplorerCurator.afterDOMLoaded = curatorScript
+  ExplorerCurator.afterDOMLoaded = `${curatorScript}\n${kingmakerVignetteOrder}`
   return ExplorerCurator
 }) satisfies QuartzComponentConstructor
