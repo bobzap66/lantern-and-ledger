f29efe8280edbe022c460d08d1ac6e83a1581145
bobzap66 <nickgpfs@gmail.com>
2026-09-14 09:00:43 -0400

    Add Kingmaker vignette Explorer order

---
+++ quartz/components/scripts/curatedExplorer.kingmakerVignettes.inline.ts
@@ -0,0 +1,24 @@
+explorerOrder["campaigns/kingmaker/vignettes"] = [
+  "intelligence-reports",
+  "moments",
+  "pitax-gazette",
+  "scholarly-journals",
+  "lantern-and-ledger-thumping-waters",
+  "seamus",
+  "matteo",
+  "lurking-hunter",
+  "elowyn-vask",
+  "ant",
+  "ally-grainger",
+  "mairi",
+  "velka",
+  "fitzroy",
+  "misty",
+  "tristian",
+  "linzi",
+]
+
+explorerOrder["campaigns/kingmaker/vignettes/scholarly-journals"] = [
+  "on-the-recent-decline-of-cooperative-fey-encounters",
+  "the-last-guardian",
+]
